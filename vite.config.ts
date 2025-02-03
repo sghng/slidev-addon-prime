fca488456648afcdf5d5c8bebb7b72c60373b846
Samuel Huang <hi@sghuang.com>
2025-02-02 23:40:17 -0500

    Include primevue/config in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "vite";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
 export default defineConfig({
+  optimizeDeps: {
+    // This is needed otherwise design tokens are not available
+    include: ["primevue/config"],
+  },
   slidev: {
     components: {
       resolvers: [PrimeVueResolver()],
