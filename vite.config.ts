573290488bca5659113c7108606a2652263451ac
Samuel Huang <hi@sghuang.com>
2025-02-04 12:27:50 -0500

    Add IconsResolver to resolvers

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="@slidev/types" />
 
 import { defineConfig } from "vite";
+import IconsResolver from "unplugin-icons/resolver";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
 export default defineConfig({
@@ -10,7 +11,27 @@ export default defineConfig({
   },
   slidev: {
     components: {
-      resolvers: [PrimeVueResolver()],
+      resolvers: [
+        /*
+         * !!! IMPORTANT !!!
+
+         * IconsResolver is required for Slidev to load icons. However, this
+         * config here is unaware of icon sets customized with icon option, and
+         * disables that option as a result.
+         * 
+         * Read more in the following resources:
+         * 
+         * - https://sli.dev/features/icons
+         * - https://github.com/unplugin/unplugin-icons
+         * - https://github.com/slidevjs/slidev/blob/main/packages/slidev/node/vite/components.ts
+         * - https://github.com/slidevjs/slidev/blob/main/packages/slidev/node/vite/index.ts
+         */
+        IconsResolver({
+          prefix: "",
+          customCollections: [],
+        }),
+        PrimeVueResolver(),
+      ],
     },
   },
 });
