148c28bba869a1ac0f5875a5855dcfd72044a367
Samuel Huang <hi@sghuang.com>
2025-02-22 21:46:20 -0500

    Modify scripts

--- src/extractor.ps1
+++ src/extractor.ps1
@@ -1,16 +1,6 @@
 param( [string] $FilePath )
 
 Get-Content $FilePath | 
-Select-String -AllMatches '(?<=<)(N[A-Z]|n-)[A-Za-z-]*' | 
+Select-String -AllMatches '(?<=<)([A-Z][A-Za-z]*' | 
 ForEach-Object { $_.Matches.Value } |
-Sort-Object -Unique |
-ForEach-Object {
-    $_ -clike 'N*' ?
-    $_ :
-    (
-        $_ -split '-' |
-        ForEach-Object { (Get-Culture).TextInfo.ToTitleCase($_) } |
-        Join-String
-    )
-} |
 Sort-Object -Unique
--- src/extractor.sh
+++ src/extractor.sh
@@ -1,11 +1,5 @@
 FILE_PATH=${1:-"slides.md"}
 
-grep -o '<\(N[A-Z]\|n-\)[A-Za-z-]*' "$FILE_PATH" \
+grep -o '<[A-Z][A-Za-z]*' "$FILE_PATH" \
   | sort -u \
-  | sed -E 's/^<//' \
-  | awk -F'-' '$1 == "n" {
-      OFS=""
-      for (i=1; i<=NF; i++)
-        $i=toupper(substr($i,1,1)) substr($i,2)
-    } 1' \
-  | sort -u
+  | sed -E 's/^<//'
