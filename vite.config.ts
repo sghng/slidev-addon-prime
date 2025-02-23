e8176f9e1518fab3e3e6ceb5e9660e98b1ce2231
Samuel Huang <hi@sghuang.com>
2025-02-22 22:16:32 -0500

    Create vite virtual module

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
-/// <reference types="@slidev/types" />
-import { defineConfig } from "vite";
+import { type Plugin, defineConfig } from "vite";
 
-export default defineConfig({
-  optimizeDeps: {
-    // This is needed otherwise design tokens are not available
-    include: ["primevue/config"],
-  },
-});
+const primePlugin = (): Plugin => {
+  const virtualModuleId = "virtual:prime";
+  const resolvedVirtualModuleId = "\0" + virtualModuleId;
+
+  return {
+    name: "prime",
+    resolveId(id) {
+      if (id === virtualModuleId) {
+        return resolvedVirtualModuleId;
+      }
+    },
+    load(id) {
+      if (id === resolvedVirtualModuleId) {
+        return process.env.PRIME_COMPONENTS
+          ? process.env.PRIME_COMPONENTS.split(",")
+              .map((name) => {
+                return (
+                  `export { default as ${name} } ` +
+                  `from "primevue/${name.toLowerCase()}";`
+                );
+              })
+              .join("\n")
+          : "export default true;"; // truthy default export => no components
+      }
+    },
+  };
+};
+
+export default defineConfig({ plugins: [primePlugin()] });
